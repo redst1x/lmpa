6a95d5a628b60f2ecb82930b0c2df17854ada0bd
redst1x <peter.kov4lev@gmail.com>
2025-11-11 11:18:07 +0300

    Update and rename freetorr.js to pbz.js

--- pbz.js
+++ pbz.js
@@ -38,25 +38,6 @@ if (Lampa.Storage.get('cub_domain_skaz')==1) checkApiAvailability(); Lampa.Stora
       }
     });
     console.log('TMDB-Proxy', 'started, enabled:', Lampa.Storage.field('proxy_tmdb'));
-	Lampa.SettingsApi.addComponent({component: 'iptvskaz',icon: "<svg height=\\"36\\" viewBox=\\"0 0 38 36\\" fill=\\"none\\" xmlns=\\"http://www.w3.org/2000/svg\\">\\n            <rect x=\\"2\\" y=\\"8\\" width=\\"34\\" height=\\"21\\" rx=\\"3\\" stroke=\\"white\\" stroke-width=\\"3\\"/>\\n            <line x1=\\"13.0925\\" y1=\\"2.34874\\" x2=\\"16.3487\\" y2=\\"6.90754\\" stroke=\\"white\\" stroke-width=\\"3\\" stroke-linecap=\\"round\\"/>\\n            <line x1=\\"1.5\\" y1=\\"-1.5\\" x2=\\"9.31665\\" y2=\\"-1.5\\" transform=\\"matrix(-0.757816 0.652468 0.652468 0.757816 26.197 2)\\" stroke=\\"white\\" stroke-width=\\"3\\" stroke-linecap=\\"round\\"/>\\n            <line x1=\\"9.5\\" y1=\\"34.5\\" x2=\\"29.5\\" y2=\\"34.5\\" stroke=\\"white\\" stroke-width=\\"3\\" stroke-linecap=\\"round\\"/>\\n        </svg>",name: 'by Skaz'});
-	Lampa.SettingsApi.addParam({
-        component: 'iptvskaz',
-        param: {
-          name: 'b_skaz',
-          type: 'trigger',
-          "default": false,
-        },
-        field: {
-          name: 'Убрать с главной трансляции',
-		  description: 'Убирает с главной трансляцию события'
-        },
-        onChange: function (value) {
-			Lampa.Noty.show('Необходимо перезайти в лампу');
-		}
-	  });
-	  
-	if (Lampa.Storage.get('b_skaz')!=true) {
-	//$.getScript(Lampa.Utils.protocol()+'oleg.skaz.tv/b.js');
 	}
 
 })();`;
